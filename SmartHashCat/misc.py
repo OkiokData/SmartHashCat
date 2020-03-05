1f57f9fa425b8892f76cf2bb0dc92711760161d7
mlahaye811 <34068053+mlahaye811@users.noreply.github.com>
2020-03-05 13:10:30 -0500

    Fix for utf 8 chars (like û) (#32)
    * Added a function to transform accent letters to hex
    
    * Update filter_rule_format.py
    
    * Update filter_rule_combinaison.py
    
    * Update filter_rule_format.py
    
    * Update filter_rule_combinaison.py
    
    * Update misc.py
    
    * Update filter_rule_combinaison.py
    
    * Update filter_rule_format.py

--- SmartHashCat/misc.py
+++ SmartHashCat/misc.py
@@ -1,5 +1,6 @@
 import datetime
 import command_runner
+import string
 
 
 def print_date_time():
@@ -21,3 +22,19 @@ def copy_file_content_to_other_file(file_in, file_out, append=True):
 
 def write_text_to_file(text, file_out, append=True):
     send_content_to_file("echo", text, file_out, append)
+
+def get_rule_format_utf_8_letter(letter, is_append):
+    hashcat_code = "$" if is_append else "^"
+    if letter in string.ascii_letters:
+        return f"{hashcat_code}{letter}"
+    
+    letter_bytes = letter.encode()
+    
+    if not is_append:
+        letter_bytes = letter_bytes[::-1]
+    
+    to_return = []
+    for i in letter_bytes:
+        to_return.append(f"{hashcat_code}\\x{hex(i)[2:]}")
+        
+    return "".join(to_return)
--- SmartHashCat/shc_filter/filter_rule_format.py
+++ SmartHashCat/shc_filter/filter_rule_format.py
@@ -1,4 +1,5 @@
 from shc_filter.filter_abstract import FilterAbstract
+import misc
 
 
 class Filter(FilterAbstract):
@@ -9,13 +10,13 @@ class Filter(FilterAbstract):
     def transform_to_prepend_hashcat_format(self, word):
         to_return = []
         for c in word[::-1]:
-            to_return.append(f'^{c}')
+            to_return.append(misc.get_rule_format_utf_8_letter(c, is_append=False))
         return ''.join(to_return)
 
     def transform_to_apend_hashcat_format(self, word):
         to_return = []
         for c in word:
-            to_return.append(f'${c}')
+            to_return.append(misc.get_rule_format_utf_8_letter(c, is_append=True))
         return ''.join(to_return)
 
     def get_results(self):
